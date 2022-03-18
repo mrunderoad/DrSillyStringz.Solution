c7cc8679dc54b4bb1c8daf3bb8d1e41ce790df48
Isaac Overstreet <isaacoverstreet3@gmail.com>
2022-03-18 13:22:24 -0700

    working on home page formatting

--- Factory/Migrations/20220318195835_Fixed.Designer.cs
+++ Factory/Migrations/20220318195835_Fixed.Designer.cs
@@ -2,14 +2,16 @@
 using Factory.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Factory.Migrations
 {
     [DbContext(typeof(FactoryContext))]
-    partial class FactoryContextModelSnapshot : ModelSnapshot
+    [Migration("20220318195835_Fixed")]
+    partial class Fixed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -11,6 +11,7 @@ namespace Factory.Models
 
     public int MachineId { get; set; }
     public string Name { get; set; }
+    public bool Fixed { get; set; }
     public virtual ICollection<EngineerMachine> JoinEntities { get; set; }
   }
 }
